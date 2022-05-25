c4c101dd266b41993ccc19e4b0c616ee294e813f
Player <player@player.to>
2022-05-25 19:17:32 +0200

    rename plugin -> extension, implement more features

--- src/main/java/net/fabricmc/loader/impl/discovery/ResultAnalyzer.java
+++ src/main/java/net/fabricmc/loader/impl/discovery/ResultAnalyzer.java
@@ -32,6 +32,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.api.extension.ModCandidate;
 import net.fabricmc.loader.api.metadata.ModDependency;
 import net.fabricmc.loader.api.metadata.version.VersionInterval;
 import net.fabricmc.loader.impl.discovery.ModResolver.ResolutionContext;
