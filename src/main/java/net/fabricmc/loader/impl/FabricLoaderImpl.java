17ce2cb31e9f479abc059d0e4ea3dfb0c4cd6802
Gamebuster <Gamebuster19901@users.noreply.github.com>
2025-07-13 01:40:31 +0000

    Correct the version

--- src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
+++ src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
@@ -79,7 +79,7 @@ public final class FabricLoaderImpl extends net.fabricmc.loader.FabricLoader {
 
 	public static final int ASM_VERSION = Opcodes.ASM9;
 
-	public static final String VERSION = "0.16.15+WilderForge.0.1.0";
+	public static final String VERSION = "0.16.14+WilderForge.0.1.0";
 	public static final String MOD_ID = "fabricloader";
 
 	public static final String CACHE_DIR_NAME = ".fabric"; // relative to game dir
